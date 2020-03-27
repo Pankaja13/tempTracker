3c5274ab09e9bc49035827bea41dd89b93ba5ade
PankajaW <Pankaja13.psd@gmail.com>
2020-03-27 16:45:07 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#import "main.hpp"
+
+const char* ssid = SSID;
+const char* password = PSK;
+
+ESP8266WebServer server(80);
+DHT dht(SENSOR_PIN, DHT22);
+
+IPAddress staticIP(192, 168, 0, 220); //ESP static ip
+IPAddress gateway(192, 168, 0, 1);   //IP Address of your WiFi Router (Gateway)
+IPAddress subnet(255, 255, 255, 0);  //Subnet mask
+IPAddress dns(8, 8, 8, 8);  //DNS
+
+HTTPClient http;
+WiFiClient client;
+
+void handleNotFound();
+
+float getTargetTemperature();
+
+void setup(void) {
+	Serial.begin(115200);
+	WiFi.mode(WIFI_STA);
+	WiFi.config(staticIP, gateway, subnet, dns);
+	WiFi.begin(ssid, password);
+
+	dht.begin();
+	http.begin(client, OPENWEATHERMAP_API_URL);
+
+	// Wait for connection
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+
+	server.onNotFound(handleNotFound);
+
+	server.on("/tempF", []() {
+		server.send(200, "text/plain", String(dht.readTemperature(true)));
+	});
+
+	server.on("/tempC", []() {
+		server.send(200, "text/plain", String(dht.readTemperature()));
+	});
+
+	server.on("/humidity", []() {
+		server.send(200, "text/plain", String(dht.readHumidity()));
+	});
+
+	server.onNotFound(handleNotFound);
+	server.begin();
+
+	Serial.println("HTTP server started");
+}
+
+void loop(void) {
+	server.handleClient();
+	delay(1);
+}
+
+float getTargetTemperature() {
+
+	int httpCode = http.GET();
+	if (httpCode > 0) {
+		if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+			String payload = http.getString();
+			Serial.println(payload);
+			StaticJsonDocument<500> data;
+			deserializeJson(data, payload);
+			String temp_max_F = data["main"]["temp_max"].as<String>();
+			return temp_max_F.toFloat();
+		}
+	}
+	Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+	return 0.0;
+}
+
+void handleNotFound() {
+	server.send(404, "text/plain",
+			"Hello! \n"
+			"Use the following paths: \n"
+			"/tempF \n"
+			"/tempC \n"
+			"/humidity  \n");
+}
---
+++ src/main.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Pankaja W on 5/14/2019.
+//
+
+#ifndef _main_H_
+#define _main_H_
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <DHT.h>
+#include "config.hpp"
+#include <ArduinoJson.h>
+
+#endif //_main_H_
